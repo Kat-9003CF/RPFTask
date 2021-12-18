1db1562f78dc4c7cab8dfa488308259d08357867
Kat Hurdley <katherine.hurdley@googlemail.com>
2021-12-18 21:34:33 +0000

    Dummy data added, password hashed

--- model/init_db.sql
+++ model/init_db.sql
@@ -6,12 +6,12 @@ CREATE TABLE usertable (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    passwordHash BINARY(64) NOT NULL,
+    passwordHash BINARY(16) NOT NULL,
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Insert some test data
 
--- INSERT INTO usertable (username, email, passwordHash)
--- VALUES
---     ('user1', 'user@email.com', 'HASHBYTES('SHA2_256', 'testPass123'));
+INSERT INTO usertable (username, email, passwordHash)
+VALUES
+    ('user1', 'user@email.com', 'MD5(testpass123)');
